338c6682b578a29f1598c6b2cb90f3e1ae894465
ann-kathrinhaas <102215660+ann-kathrinhaas@users.noreply.github.com>
2022-11-17 12:47:38 +0100

    L06

---
+++ Aufgaben/L06_Einkaufsliste_DatabaseServer/generateContent.ts
@@ -0,0 +1,85 @@
+namespace L06_Einkaufsliste {
+
+    export interface Item {
+        name: string;
+        amount: number;
+        comment: string;
+        date: string;
+        checked: boolean;
+    }
+
+    export interface Data {
+        [entry: string]: Item[];
+    }
+
+    export function generateContent(_data: Data): void {
+
+        let itemList: HTMLElement = <HTMLFormElement>document.querySelector("#itemList");
+        
+        console.log("Entry");
+
+        for (let i: number = 0; i < _data.Entry.length; i++) {
+            
+            // Div für Item Infos
+            let itemDiv: HTMLDivElement = document.createElement("div");
+            itemDiv.classList.add("itemInfo");
+            itemList.appendChild(itemDiv);
+            itemDiv.addEventListener("click", deleteItem);
+            itemDiv.addEventListener("click", checkItem);
+
+            // Checkbox
+            let itemCheck: HTMLInputElement = document.createElement("input");
+            itemCheck.setAttribute("type", "checkbox");
+            itemCheck.classList.add("checkbox");
+            itemCheck.checked = _data.Entry[i].checked;
+            itemDiv.appendChild(itemCheck);
+
+            // Name
+            let itemName: HTMLLabelElement = document.createElement("label");
+            itemName.classList.add("name");
+            itemName.innerHTML = _data.Entry[i].name;
+            itemDiv.appendChild(itemName);
+
+            // Amount
+            let itemAmount: HTMLElement = document.createElement("p");
+            itemAmount.classList.add("amount");
+            itemAmount.innerHTML = _data.Entry[i].amount.toString();
+            itemDiv.appendChild(itemAmount);
+
+            // Edit Button
+            let editButton: HTMLButtonElement = document.createElement("button");
+            editButton.classList.add("editButton");
+            editButton.innerHTML = '<i class = "pen fas fa-pen"></i>';
+            itemDiv.appendChild(editButton);
+
+            // Delete Button
+            let deleteButton: HTMLButtonElement = document.createElement("button");
+            deleteButton.classList.add("deleteButton");
+            deleteButton.innerHTML = '<i class = "trash fas fa-trash-alt"></i>';
+            itemDiv.appendChild(deleteButton);
+
+            // Date
+            let itemDate: HTMLInputElement = document.createElement("input");
+            itemDate.classList.add("date");
+            itemDate.setAttribute("placeholder", "Date");
+            itemDate.value = _data.Entry[i].date;
+            itemDiv.appendChild(itemDate);
+
+            // Comment
+            let itemComment: HTMLInputElement = document.createElement("input");
+            itemComment.classList.add("comment");
+            itemComment.setAttribute("placeholder", "Comment");
+            itemComment.setAttribute("cols", "30");
+            itemComment.setAttribute("rows", "1");
+            itemComment.value = _data.Entry[i].comment;
+            itemDiv.appendChild(itemComment);
+           
+        }
+        
+    }
+
+
+    
+    
+
+}
---
+++ Aufgaben/L06_Einkaufsliste_DatabaseServer/script.js
@@ -0,0 +1,125 @@
+"use strict";
+/*
+Aufgabe: L06 Einkaufsliste Database Server
+Name: Ann-Kathrin Haas
+Matrikel: 271111
+Datum: 17.11.22
+Quellen: -
+*/
+var L06_Einkaufsliste;
+(function (L06_Einkaufsliste) {
+    window.addEventListener("load", handleLoad);
+    async function handleLoad() {
+        console.log("Init");
+        let response = await fetch("https://ann-kathrinhaas.github.io/EIA2-WiSe22/Aufgaben/L05_Einkaufsliste_Client/data.json");
+        let content = await response.text();
+        let data = JSON.parse(content);
+        L06_Einkaufsliste.generateContent(data); // erstellt Beispieleinträge mit Inhalten aus data.json
+        let addButton = document.querySelector("#addButton");
+        addButton.addEventListener("click", addItem);
+    }
+    function addItem() {
+        console.log("Add Item");
+        let itemList = document.querySelector("#itemList");
+        let inputName = document.querySelector("#newItem");
+        let inputAmount = document.querySelector("#newAmount");
+        let inputComment = document.querySelector("#newComment");
+        // Div für Item Infos
+        let itemDiv = document.createElement("div");
+        itemDiv.classList.add("itemInfo");
+        itemList.appendChild(itemDiv);
+        itemDiv.style.position = "relative";
+        itemDiv.style.top = "20px";
+        itemDiv.addEventListener("click", deleteItem);
+        itemDiv.addEventListener("click", checkItem);
+        // Checkbox
+        let itemCheck = document.createElement("input");
+        itemCheck.setAttribute("type", "checkbox");
+        itemCheck.classList.add("checkbox");
+        itemDiv.appendChild(itemCheck);
+        // Name
+        let itemName = document.createElement("p");
+        itemName.classList.add("name");
+        itemName.innerHTML = inputName.value;
+        itemDiv.appendChild(itemName);
+        inputName.value = "";
+        // Amount
+        let itemAmount = document.createElement("p");
+        itemAmount.classList.add("amount");
+        itemAmount.innerHTML = inputAmount.value;
+        itemName.appendChild(itemAmount);
+        inputAmount.value = "";
+        // Edit Button
+        let editButton = document.createElement("button");
+        editButton.classList.add("editButton");
+        editButton.innerHTML = '<i class = "pen fas fa-pen"></i>';
+        itemName.appendChild(editButton);
+        editButton.style.position = "absolute";
+        editButton.style.right = "-148px";
+        editButton.style.top = "0px";
+        // Delete Button
+        let deleteButton = document.createElement("button");
+        deleteButton.classList.add("deleteButton");
+        deleteButton.innerHTML = '<i class = "trash fas fa-trash-alt"></i>';
+        itemName.appendChild(deleteButton);
+        deleteButton.style.position = "absolute";
+        deleteButton.style.right = "-168px";
+        deleteButton.style.top = "0px";
+        // Date
+        let itemDate = document.createElement("p");
+        itemDate.classList.add("date");
+        itemDate.setAttribute("placeholder", "Date");
+        itemDate.innerHTML = "Date";
+        itemDiv.appendChild(itemDate);
+        // Comment
+        let itemComment = document.createElement("input");
+        itemComment.classList.add("comment");
+        itemComment.setAttribute("placeholder", "Comment");
+        itemComment.setAttribute("cols", "30");
+        itemComment.setAttribute("rows", "1");
+        itemComment.value = inputComment.value;
+        itemDiv.appendChild(itemComment);
+        inputComment.value = "";
+        sendDataToServer();
+    }
+    function checkItem(_event) {
+        console.log("Check Item");
+        let target = _event.target;
+        let currentTarget = _event.currentTarget;
+        if (target.classList.contains("checkbox")) {
+            let date = new Date();
+            let day = date.getDate();
+            let month = date.getMonth() + 1;
+            let year = date.getFullYear();
+            let checkDate = day + "." + month + "." + year;
+            console.log("Check Date: " + checkDate);
+            console.log(currentTarget);
+            let dateField = currentTarget.querySelector(".date");
+            dateField.setAttribute("placeholder", checkDate);
+            console.log(dateField);
+            dateField.innerHTML = checkDate;
+        }
+        sendDataToServer();
+    }
+    L06_Einkaufsliste.checkItem = checkItem;
+    function deleteItem(_event) {
+        console.log("Delete Item");
+        let target = _event.target;
+        let currentTarget = _event.currentTarget;
+        let parentElement = currentTarget.parentElement;
+        if (target.classList.contains("deleteButton") || target.classList.contains("trash")) {
+            parentElement.removeChild(currentTarget);
+        }
+        sendDataToServer();
+    }
+    L06_Einkaufsliste.deleteItem = deleteItem;
+    async function sendDataToServer() {
+        console.log("Send Data To Server");
+        let formData = new FormData(document.forms[0]);
+        let url = "einkaufsliste.html";
+        let query = new URLSearchParams(formData);
+        await fetch(url + "?" + query.toString());
+        alert("Data Sent");
+    }
+})(L06_Einkaufsliste || (L06_Einkaufsliste = {}));
+//# sourceMappingURL=script.js.map
